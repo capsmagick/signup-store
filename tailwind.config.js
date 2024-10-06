3d604f832f39f961a784479111a12a66afce558e
mrudulks <devmrudulks@gmail.com>
2024-10-06 23:47:08 +0530

    intial project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+import { colors } from 'tailwindcss';
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {}
-  },
-  plugins: []
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				...colors,
+				head: '#222',
+				primary: '#FAF9F8',
+				second: '#767676',
+				third: '#B9A16B',
+				fourth: '#EEEEEE',
+				footer: '#E4E4E4',
+				'second-red': '#C32929'
+			}
+		}
+	},
+	plugins: []
 };
-
